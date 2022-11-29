3e98d58433960dc80bd1e47261ca1c065bda291d
szaboolivia <szaboolivia@EOTVOS2.LOCAL>
2022-11-29 14:34:33 +0100

    ch, lat, lon

--- app/Http/Controllers/Aktualis.php
+++ app/Http/Controllers/Aktualis.php
@@ -19,6 +19,25 @@ class Aktualis extends Controller
         INNER JOIN homersekletek ON (termek.t_id=homersekletek.t_id) 
         GROUP BY termek.t_id 
         ORDER BY homersekletek.h_id DESC");
+        $lat ="46.08333";
+        $lon = "18.23333";
+        $i = $this->idojaras($lat,$lon);
         return view("welcome",["aktualis"=> $aktualis]);
+    }
+    public function idojaras($lat,$lon){
+        $apiKey ="360fc091e9b3af2707606bdacba0aa3";
+        $url ="https://api.openweathermap.org/data/2.5/weather?lat=".$lat."&lon".$lon."&appid=".$apiKey;
+
+        $ch =curl_init();
+        curl_setopt($ch,CURLOPT_HEADER,0);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+        curl_setopt($ch,CURLOPT_VERBOSE,0);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+        $valasz = curl_exec($ch);
+        curl_close($ch);
+        $idojarasAdatok = json_decode($valasz,true);
+        return $idojarasAdatok;
     }
 }
